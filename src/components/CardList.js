4bc022ed3e678760c83a073c9426a7c27ce45585
Vivienne Chen <viviennechen@Viviennede-MBP.fritz.box>
2021-04-22 21:35:06 +0200

    Cleanup ignored files and add comments

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Card from './Card';
 
+// CarList is Card's parents. Using map method to iterate all robots.
+// Each child in an array or iterator should have a unique "key" prop. 
+
 const CardList = ({robots}) => {
     return (
         <div>
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// To keep the SearchBox at the top.
+// Using props.children to wrap the CardList
+
 const Scroll = (props) => {
     return (
         <div style={{overflowY: 'scroll', border: '1px solid black', height: '800px'}}>
